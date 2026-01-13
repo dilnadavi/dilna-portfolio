d5d9cf12406f53fd9a4f180c78f1167730c5790b
Dilna <dilnadavi41@gmail.com>
2026-01-12 21:28:01 -0800

    add social media icons

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.css'
 import illustration from '../../assets/temp_icon.png'
 import name_logo from '../../assets/name_logo.png'
+import linkedin_icon from '../../assets/linkedin_logo.png' 
+import github_icon from '../../assets/github_logo.png'
 
 const Header = () => {
   return (
@@ -16,6 +18,14 @@ const Header = () => {
 
         <h2 className="subtitle">
            <p className='about-me-description'>a BSc Computer Science student exploring software, <span>impact</span>, and innovation.</p>
+                     <div className="social-icons">
+            <a href="https://www.linkedin.com/in/dilna-davi/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedin_icon} className="icon" />
+            </a>
+            <a href="https://github.com/dilnadavi" target="_blank" rel="noopener noreferrer">
+              <img src={github_icon} className="icon github" />
+            </a>
+          </div>
         </h2>
       </div>
 
