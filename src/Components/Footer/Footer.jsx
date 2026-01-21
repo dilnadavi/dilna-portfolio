3d0773c5d66bdfcea6d21f63bae4c2c0b1ed479f
Dilna <dilnadavi41@gmail.com>
2026-01-20 22:51:20 -0800

    make all components responsive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer-container'>
         <p style={{textAlign: 'center', padding: '20px', fontSize: '10px', color: '#888'}}>© 2026 Dilna Davi. All rights reserved.</p>
     </div>
   )
