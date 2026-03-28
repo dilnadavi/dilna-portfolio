03250f087b8ab9197aa38aabd50e29c3f763401d
Dilna <dilnadavi41@gmail.com>
2026-03-27 19:06:00 -0700

    change navbar and favicon

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,19 +2,12 @@ import { NavbarData } from "./NavbarData.jsx"
 import './Navbar.css';
 
 const Navbar = () => {
-  const scrollToSection = (id) => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <div className='navbar'>
       <div className="navbar-box">
         <ul className="nav-menu">
           {NavbarData.map((val, key) => (
-            <li key={key} onClick={() => scrollToSection(val.target)}>
+            <li key={key} onClick={() => window.location.reload()} style={{ cursor: 'pointer' }}>
               <img
                 src={val.icon}
                 style={{ width: val.size, height: val.size }}
@@ -26,6 +19,5 @@ const Navbar = () => {
     </div>
   );
 };
-
 
 export default Navbar
--- src/Components/Navbar/NavbarData.jsx
+++ src/Components/Navbar/NavbarData.jsx
@@ -1,32 +1,11 @@
 import React from "react"
-import person_logo from '../../assets/person_svg.svg'
-import location_logo from '../../assets/map_svg.svg'
-import project_logo from '../../assets/folder_svg.svg'
-import info_logo from '../../assets/design_svg.svg'
+import person_logo from '../../assets/plant.svg'
 
 export const NavbarData = [
     {
         key: "person_logo",
         icon: person_logo,
         size: 30,
-        target: "header"
+        target: "#"
     },
-    {
-        key: "location_logo",
-        icon: location_logo,
-        size: 30,
-        target: "roadmap"
-    },
-    {
-        key: "project_logo",
-        icon: project_logo,
-        size: 30,
-        target: "projects"
-    },
-    {
-        key: "info_logo",
-        icon: info_logo,
-        size: 30,
-        target: "extra"
-    }
 ];
