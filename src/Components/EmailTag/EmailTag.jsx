28b4358dcabee0782a34f6e695eb0a1c2ea8005b
Dilna <dilnadavi41@gmail.com>
2026-03-27 17:57:10 -0700

    change header text and add instagram link

--- src/Components/EmailTag/EmailTag.jsx
+++ src/Components/EmailTag/EmailTag.jsx
@@ -4,7 +4,7 @@ import './EmailTag.css'
 const EmailTag = () => {
   return (
     <div className='email-tag'>
-        <p>Please don't hesitate to reach out!</p>
+        <p>Don't hesitate to reach out!</p>
         <a href="mailto:dilnadavi41@gmail.com" className='email'>dilnadavi41@gmail.com</a>
     </div>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,12 +4,13 @@ import illustration from '../../assets/webp/portfolio_sketch.webp'
 import name_logo from '../../assets/webp/name_logo.webp'
 import linkedin_icon from '../../assets/webp/linkedin_logo.webp' 
 import github_icon from '../../assets/webp/github_logo.webp'
+import instagram_icon from '../../assets/webp/instagram_logo_alt.webp'
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-left">
-        <h1 className="intro-text"><span>Hello! </span>My name is</h1>
+        <h1 className="about-me-description"><span>Hello! </span>My name is</h1>
 
         <img
           className="name-logo"
@@ -18,10 +19,8 @@ const Header = () => {
 
         <h2 className="subtitle">
           <p className='about-me-description'>
-            a BSc Computer Science student exploring 
-            <span className="code-font">software,</span> 
-            <span className='impact-font'>impact</span>, and 
-            <span className="segoe-font"> innovation</span>.
+            and I am a BSc Computer Science student based in 
+            <span className='impact-font'> Vancouver, B.C. </span>
           </p>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/dilna-davi/" target="_blank" rel="noopener noreferrer">
@@ -29,6 +28,9 @@ const Header = () => {
             </a>
             <a href="https://github.com/dilnadavi" target="_blank" rel="noopener noreferrer">
               <img src={github_icon} className="icon github" />
+            </a>
+            <a href="https://www.instagram.com/dillpicklx/" target="_blank" rel="noopener noreferrer">
+              <img src={instagram_icon} className="icon instagram" />
             </a>
           </div>
         </h2>
